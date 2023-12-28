549a6a8688c606494429cf3ef77c96fc615040a7
suucong <sophia021210@naver.com>
2023-12-29 04:44:22 +0900

    fix: 로그인 오류 수정

--- ZigzagBE/src/main/java/com/zigzag/app/config/auth/WebConfig.java
+++ ZigzagBE/src/main/java/com/zigzag/app/config/auth/WebConfig.java
@@ -10,11 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOriginPatterns("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true)
                 .exposedHeaders("Authorization")
-                .exposedHeaders("Access-Control-Allow-Origin");
+                .allowCredentials(true);
     }
 }
